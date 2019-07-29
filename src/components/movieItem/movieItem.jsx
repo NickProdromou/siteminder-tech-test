f5665edbefc0c4c889723df5c93ba820e632000b
NickProdromou <nick.prodromou1@gmail.com>
2019-07-29 16:36:49 +1000

    test utils, movieItem component and unit tests

--- src/components/movieItem/movieItem.jsx
+++ src/components/movieItem/movieItem.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './movieItem.module.scss';
+import StarIcon from '../icons/star';
 
-export default function movieItem() {
-  return <div>movie tile or item view</div>;
+export default function movieItem({ Title, Year, isSelected }) {
+  return (
+    <article className={styles.Root}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.titleText} data-test-id="title-text">
+          {Title}
+        </h1>
+      </div>
+      <div className={styles.infoContainer}>
+        {isSelected && <StarIcon className={styles.selectedIcon} />}
+        <p className={styles.releaseYear} data-test-id="release-year-text">
+          {Year}
+        </p>
+      </div>
+    </article>
+  );
 }
 
-movieItem.propTypes = {};
+movieItem.propTypes = {
+  Title: PropTypes.string.isRequired,
+  Year: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool
+};
+
+movieItem.defaultProps = {
+  isSelected: false
+};
--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,12 @@
 import sinon from 'sinon';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 chai.use(sinonChai);
+
+configure({ adapter: new Adapter() });
 
 global.sinon = sinon;
 global.expect = chai.expect;
